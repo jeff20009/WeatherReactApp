75a392a984ad46011cc1b46ba07f9febce43ae26
Jeffchiucp <jeffchiu2000@gmail.com>
2017-08-31 16:33:45 -0700

    add redux components containers

---
+++ src/components/chart/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import _ from 'lodash'
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines'
+
+function average(data){
+    return _.round(_.sum(data)/data.length);
+}
+
+export default (props) => {
+    return (
+        <div>
+            <Sparklines data={props.data} width={90} height= {50}>
+                <SparklinesLine color= { props.color }/>
+                <SparklinesReferenceLine type="avg" />
+            </Sparklines>
+            <div>{average(props.data)} {props.units}</div>
+        </div>
+    )
+}
